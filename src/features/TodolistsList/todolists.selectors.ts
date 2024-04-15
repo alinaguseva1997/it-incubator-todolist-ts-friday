96c133efe07933aad1153570293220de3aa6d4cb
alinaguseva1997 <alinagorbunova93@gmail.com>
2024-04-15 23:57:51 +0300

    fix

--- src/features/TodolistsList/todolists.selectors.ts
+++ src/features/TodolistsList/todolists.selectors.ts
@@ -1,3 +1,2 @@
-import { AppRootStateType } from "app/store";
-
-export const selectTodolists = (state: AppRootStateType) => state.todolists;
+import { AppRootStateType } from "app/store"
+export const selectTodolists = (state: AppRootStateType) => state.todolists
