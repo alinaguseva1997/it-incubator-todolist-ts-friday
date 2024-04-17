86ac08bfd4e9e9f670737d9a56f1837f53bf74f6
alinaguseva1997 <alinagorbunova93@gmail.com>
2024-04-17 13:14:13 +0300

    add thunkTryCatch utils in tasks.reducer

---
+++ src/common/utils/thunkTryCatch.ts
@@ -0,0 +1,26 @@
+import { AppDispatch, AppRootStateType } from "app/store"
+import { handleServerNetworkError } from "common/utils/handle-server-network-error"
+import { BaseThunkAPI } from "@reduxjs/toolkit/dist/createAsyncThunk"
+import { appActions } from "app/app.reducer"
+import { BaseResponseType } from "common/types"
+
+export const thunkTryCatch = async <T>(
+  thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>,
+  logic: () => Promise<T>,
+): Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>> => {
+  const { dispatch, rejectWithValue } = thunkAPI
+  dispatch(appActions.setAppStatus({ status: "loading" }))
+  try {
+    return await logic()
+  } catch (e) {
+    handleServerNetworkError(e, dispatch)
+    return rejectWithValue(null)
+  } finally {
+    dispatch(appActions.setAppStatus({ status: "idle" }))
+  }
+}
+//Здесь мы передаем thunkAPI и функцию logic в thunkTryCatch. (logic - это именно положительный кейс, который сидит в ветке try)
+//Функция logic - это функция, которую мы хотим выполнить с помощью try. Мы использовали анонимную функцию внутри thunkTryCatch для выполнения logic.
+//Функция thunkTryCatch возвращает результат выполнения logic.
+//Если во время выполнения logic произошла ошибка, мы обрабатываем ее в блоке catch. Затем мы заканчиваем выполнение thunkTryCatch, устанавливая статус приложения в первоначальное состояние idle.
+// Это позволяет нам избежать дублирования кода и повторного использования catch блоков в каждом из thunk.
